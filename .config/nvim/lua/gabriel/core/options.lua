763a0bee02525c2e5394b9ec80fb2db79bed1945
Gabriel Paura <gabrielpaura@Zeus.local>
2025-07-25 10:14:15 -0300

    Cursor plugin

--- .config/nvim/lua/gabriel/core/options.lua
+++ .config/nvim/lua/gabriel/core/options.lua
@@ -13,16 +13,16 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 opt.cursorline = true
 
@@ -30,7 +30,7 @@ opt.cursorline = true
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -51,19 +51,19 @@ vim.cmd("filetype plugin indent on")
 
 -- Set up filetype mappings for React files
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.jsx" },
-    callback = function()
-      vim.bo.filetype = "javascriptreact"
-    end,
-  })
-  
-  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.tsx" },
-    callback = function()
-      vim.bo.filetype = "typescriptreact"
-    end,
-  })
-  
-  -- Ensure TreeSitter uses the correct parsers for React filetypes
-  vim.treesitter.language.register('javascript', 'javascriptreact')
-  vim.treesitter.language.register('tsx', 'typescriptreact')
+  pattern = { "*.jsx" },
+  callback = function()
+    vim.bo.filetype = "javascriptreact"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.tsx" },
+  callback = function()
+    vim.bo.filetype = "typescriptreact"
+  end,
+})
+
+-- Ensure TreeSitter uses the correct parsers for React filetypes
+vim.treesitter.language.register('javascript', 'javascriptreact')
+vim.treesitter.language.register('tsx', 'typescriptreact')
